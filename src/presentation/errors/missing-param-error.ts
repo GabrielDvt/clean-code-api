540ca630bca354ba8d4d451b8411738b6f3275f8
gabriel <gabriel.dvt@Hotmail.com>
2022-06-14 11:50:56 -0300

    feat: ensure signup controller returns 400 if email is invalid

--- src/presentation/errors/missing-param-error.ts
+++ src/presentation/errors/missing-param-error.ts
@@ -1,6 +1,6 @@
 export class MissingParamError extends Error {
   constructor (paraName: string) {
     super(`Missing param: ${paraName}`)
-    this.name = paraName
+    this.name = 'MissingParamError'
   }
 }
