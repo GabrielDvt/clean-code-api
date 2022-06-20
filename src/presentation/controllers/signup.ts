99bda817e8d75d06d2c0306f746a0cf2062c8ed9
gabriel <gabriel.dvt@Hotmail.com>
2022-06-20 15:31:39 -0300

    refactor: remove duplicated code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,6 +1,6 @@
 import { MissingParamError, InvalidParamError } from '../errors'
 import { badRequest, serverError } from './../helpers/httpHelper'
-import { Controller, EmailValidator, HttpRequest, HttpResponse} from './../protocols'
+import { Controller, EmailValidator, HttpRequest, HttpResponse } from './../protocols'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
@@ -12,17 +12,15 @@ export class SignUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     try {
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+      const { email, password, passwordConfirmation } = httpRequest.body
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
           return badRequest(new MissingParamError(field))
         }
       }
-
-      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) return badRequest(new InvalidParamError('passwordConfirmation'))
-      const isEmailValid = this.emailValidator.isValid(httpRequest.body.email)
-
+      if (password !== passwordConfirmation) return badRequest(new InvalidParamError('passwordConfirmation'))
+      const isEmailValid = this.emailValidator.isValid(email)
       if (!isEmailValid) return badRequest(new InvalidParamError('email'))
-
       return {
         body: '',
         statusCode: 200
